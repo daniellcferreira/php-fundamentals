05036c58b0c6e1cea08534ecc6615ec0f19e878a
Daniel Ferreira <dcfer2017@gmail.com>
2025-10-20 11:05:35 -0300

    feat: adicionar exemplo de uso de variáveis e cálculos em variaveis.php

---
+++ variaveis.php
@@ -0,0 +1,16 @@
+<?php
+
+  // Exemplo de variáveis
+  $valorDia = 80; 
+  $gorgetas = 40;
+  $diaria = $valorDia + $gorgetas;
+
+  echo "O valor total da 1º diária é : R$ " . $diaria . "\n";
+
+  $gorgetas = 50;
+  $diaria = $valorDia + $gorgetas;
+
+ 
+  echo "O valor total da 2ª diária é : R$ " . $diaria;
+
+?>
