6e95a28324775e422b2f999ed70ee54bced11092
Daniel Ferreira <dcfer2017@gmail.com>
2025-10-21 15:03:31 -0300

    feat: adicionar exemplo de estrutura condicional if...else em PHP

---
+++ condicional-ifelse.php
@@ -0,0 +1,15 @@
+<?php
+
+  // Exemplo de condicional if...else em PHP
+  $nota = 4;
+
+  if ($nota >= 7) {
+    echo "Aluno aprovado!";
+  } else if ($nota >= 5) {
+    echo "Aluno em recuperação!";
+  } else {
+    echo "Aluno reprovado!";
+  }
+
+
+?>
