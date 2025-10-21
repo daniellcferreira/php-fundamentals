009dc1bf1f18ca717d6662806c5f217bb7ab884e
Daniel Ferreira <dcfer2017@gmail.com>
2025-10-21 15:20:41 -0300

    feat: adicionar exemplos de laços de repetição for em PHP

---
+++ repeticao-for.php
@@ -0,0 +1,20 @@
+<?php
+
+  // Exemplo de laços de repetição 'for' em PHP
+  for ($i = 1; $i <= 10; $i++) {
+    echo "Número atual: " . $i . "\n";
+  }
+
+  for ($j = 10; $j >= 1; $j--) {
+    echo "Contagem regressiva: " . $j . "\n";
+  }
+
+  for ($k = 0; $k <= 20; $k += 2) {
+    echo "Número par: " . $k . "\n";
+  }
+
+  for ($l = 1; $l <= 20; $l += 2) {
+    echo "Número impar: " . $l . "\n";
+  }
+
+?>
