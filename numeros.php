482dde27c743f162e1bbf55d423bb55c7e6eb565
Daniel Ferreira <dcfer2017@gmail.com>
2025-10-21 11:45:30 -0300

    feat: adicionar exemplos de números e tipos numéricos em PHP

---
+++ numeros.php
@@ -0,0 +1,14 @@
+<?php
+
+  // Exemplo de números em PHP
+  $number = 10;
+  $number2 = 2.5;
+  $number3 = "20";
+  $number4 = -5;
+
+  var_dump($number);
+  var_dump($number2);
+  var_dump($number3);
+  var_dump($number4);
+
+?>
--- strings.php
+++ strings.php
@@ -1,5 +1,6 @@
 <?php
 
+  // Exemplo de strings em PHP
   $texto = "<h1> Exemplo texto </h1>";
   $texto2 = "<p> Exemplo parágrafo </p>";
   $texto3 = "<div> Exemplo div </div>";
