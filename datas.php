92d428a27842d7fd18ed6b7b495690476fa7c96a
Daniel Ferreira <dcfer2017@gmail.com>
2025-10-21 11:52:50 -0300

    feat: adicionar exemplos de manipulação de datas e horas em PHP

---
+++ datas.php
@@ -0,0 +1,15 @@
+<?php
+
+  // Exemplo de datas em PHP
+  echo date("d\n");
+  echo date("m\n");
+  echo date("Y\n");
+  echo date("d-m-Y\n");
+
+  // Exemplo de horas em PHP
+  echo date("H:i:s\n");
+
+  date_default_timezone_set("America/Sao_Paulo");
+  echo date("H:i:s\n");
+
+?>
