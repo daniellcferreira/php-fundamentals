360e93fc2b3f6c03d508d387742a6955860b5f5a
Daniel Ferreira <dcfer2017@gmail.com>
2025-10-21 11:41:32 -0300

    feat: adicionar exemplos de strings e saída HTML em PHP

---
+++ strings.php
@@ -0,0 +1,13 @@
+<?php
+
+  $texto = "<h1> Exemplo texto </h1>";
+  $texto2 = "<p> Exemplo parágrafo </p>";
+  $texto3 = "<div> Exemplo div </div>";
+  $texto4 = "Exemplo de string simples";
+
+  echo $texto;
+  echo $texto2;
+  echo $texto3;
+  echo $texto4;
+
+?>
