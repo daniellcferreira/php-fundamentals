6fab5e6414a7026d374eb2ccd8510b148484ba45
Daniel Ferreira <dcfer2017@gmail.com>
2025-11-07 10:20:10 -0300

    feat: adicionar propriedades e método de exibição na classe ContaBancaria

--- poo/src/ContaBancaria.php
+++ poo/src/ContaBancaria.php
@@ -1,8 +1,26 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App;
 
 class ContaBancaria {
+
+  public string $banco = "Banco Inter";
+  public string $nomeTitular = "João Silva";
+  public string $numeroAgencia = "8244";
+  public string $numeroConta = "12345-6";
+  public float $saldo = 0;
+
+  public function exibirDadosConta(): array {
+    return [
+      'banco' => $this->banco,
+      'nomeTitular' => $this->nomeTitular,
+      'numeroAgencia' => $this->numeroAgencia,
+      'numeroConta' => $this->numeroConta,
+      'saldo' => $this->saldo,
+    ];
+  }
 
 }
 
