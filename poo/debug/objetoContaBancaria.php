1a393d240d33d32f9faba2469f017f8b2638d0fc
Daniel Ferreira <dcfer2017@gmail.com>
2025-11-07 14:59:34 -0300

    refactor: aplicar encapsulamento na classe ContaBancaria e atualizar script de depuração

--- poo/debug/objetoContaBancaria.php
+++ poo/debug/objetoContaBancaria.php
@@ -5,4 +5,11 @@ require __DIR__ . '/../vendor/autoload.php';
 use App\ContaBancaria;
 
 $conta = new ContaBancaria();
+
+$conta->setBanco("Banco do Brasil");
+$conta->setNomeTitular("Maria Oliveira");
+$conta->setNumeroAgencia("4321");
+$conta->setNumeroConta("65432-1");
+$conta->setSaldo(1500.75);
+
 var_dump($conta->exibirDadosConta());
