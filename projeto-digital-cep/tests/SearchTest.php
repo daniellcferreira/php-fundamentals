0aeb26ad4276ebb2708a4f5958ece1b7f91de2e5
Daniel Ferreira <dcfer2017@gmail.com>
2025-11-14 14:59:20 -0300

    test: corrigir testes de CEP para considerar apenas campos esperados no retorno

--- projeto-digital-cep/tests/SearchTest.php
+++ projeto-digital-cep/tests/SearchTest.php
@@ -4,29 +4,50 @@ use PHPUnit\Framework\TestCase;
 use ProjetoDigitalCep\Search;
 
 class SearchTest extends TestCase {
-  public function testGetAddressFromZipcode() {
 
-    // Instancia a classe Search
-    $resultado = new Search;
+  /**
+   * @dataProvider dadosTeste
+   */
+  public function testGetAddressFromZipcode(string $input, array $esperado) {
 
-    // Faz a consulta real para o ViaCEP
-    $resultado = $resultado->getAddressFromZipcode('03624010');
+    $search = new Search;
+    $resultado = $search->getAddressFromZipcode($input);
 
-    // Array esperado com apenas os campos reais para este CEP
-    $esperado = [
-      "cep"         => "03624-010",
-      "logradouro"  => "Rua Luís Asson",
-      "complemento" => "",
-      "bairro"      => "Vila Buenos Aires",
-      "localidade"  => "São Paulo",
-      "uf"          => "SP",
-    ];
-
-    // Verifica apenas esses campos específicos
-    foreach ($esperado as $campo => $valor) {
-      $this->assertEquals($valor, $resultado[$campo]);
-    }
+    // Compara apenas as chaves fornecidas em $esperado
+    $this->assertEquals($esperado, array_intersect_key($resultado, $esperado));
   }
 
-  
+  public static function dadosTeste() {
+    return [
+      [
+        "03624010",
+        [
+          "cep" => "03624-010",
+          "logradouro" => "Rua Luís Asson",
+          "complemento" => "",
+          "bairro" => "Vila Buenos Aires",
+          "localidade" => "São Paulo",
+          "uf" => "SP",
+          "unidade" => "",
+          "ibge" => "3550308",
+          "gia" => "1004"
+        ]
+      ],
+      [
+        "01001000",
+        [
+          "cep" => "01001-000",
+          "logradouro" => "Praça da Sé",
+          "complemento" => "lado ímpar",
+          "bairro" => "Sé",
+          "localidade" => "São Paulo",
+          "uf" => "SP",
+          "unidade" => "",
+          "ibge" => "3550308",
+          "gia" => "1004"
+        ]
+      ]
+    ];
+  }
+
 }
