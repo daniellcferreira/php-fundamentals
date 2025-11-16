b2b2af1972876a6a1f5ff6b1c872dabe223bd92f
Daniel Ferreira <dcfer2017@gmail.com>
2025-11-16 10:22:47 -0300

    docs: criar README com badges, descrição e funcionalidades do projeto

---
+++ projeto-digital-cep/README.md
@@ -0,0 +1,18 @@
+# Projeto Digital CEP
+
+![PHP](https://img.shields.io/badge/PHP-Backend-777BB4?style=flat-square&logo=php)
+![Composer](https://img.shields.io/badge/Composer-Gerenciador_de_Dependências-885630?style=flat-square&logo=composer)
+![PHPUnit](https://img.shields.io/badge/PHPUnit-Testes-36648B?style=flat-square&logo=php)
+![CodeSniffer](https://img.shields.io/badge/CodeSniffer-Linter-15AABF?style=flat-square&logo=php)
+
+## Descrição
+Projeto para consulta de CEP utilizando a API ViaCEP.  
+Permite buscar informações de endereço a partir de um CEP informado, retornando os dados em formato de array.  
+Inclui testes automatizados com PHPUnit.
+
+## Funcionalidades
+- Consulta de CEP pela API ViaCEP.
+- Sanitização automática do CEP.
+- Retorno estruturado em array.
+- Testes automatizados com PHPUnit.
+- Suporte a autoload via Composer.
--- projeto-digital-cep/src/Search.php
+++ projeto-digital-cep/src/Search.php
@@ -5,24 +5,25 @@
 namespace ProjetoDigitalCep;
 
 // Define a classe responsável por buscar endereços a partir de um CEP.
-class Search {
-
+class Search
+{
   // URL base da API ViaCEP, usada como ponto de partida para as requisições.
-  private $url = "https://viacep.com.br/ws/";
+    private $url = "https://viacep.com.br/ws/";
 
   // Método público que recebe um CEP e retorna um array com os dados do endereço.
-  public function getAddressFromZipcode(string $zipcode): array {
+    public function getAddressFromZipcode(string $zipcode): array
+    {
 
-    // Remove todos os caracteres que não sejam números do CEP recebido.
-    // Exemplo: "01001-000" -> "01001000"
-    $zipcode = preg_replace('/[^0-9]/im', '', $zipcode);
+      // Remove todos os caracteres que não sejam números do CEP recebido.
+      // Exemplo: "01001-000" -> "01001000"
+        $zipcode = preg_replace('/[^0-9]/im', '', $zipcode);
 
-    // Faz a requisição HTTP GET para a API ViaCEP.
-    // A resposta é um JSON com os dados do endereço.
-    $get = file_get_contents($this->url . $zipcode . "/json");
-    
-    // Converte o JSON retornado em um objeto PHP e, em seguida, para um array associativo.
-    // Retorna o array final para quem chamou o método.
-    return (array) json_decode($get);
-  }
+      // Faz a requisição HTTP GET para a API ViaCEP.
+      // A resposta é um JSON com os dados do endereço.
+        $get = file_get_contents($this->url . $zipcode . "/json");
+
+      // Converte o JSON retornado em um objeto PHP e, em seguida, para um array associativo.
+      // Retorna o array final para quem chamou o método.
+        return (array) json_decode($get);
+    }
 }
