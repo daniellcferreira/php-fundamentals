d20c270747217ee443b97496fc327d6a9c39ce3f
Daniel Ferreira <dcfer2017@gmail.com>
2025-11-17 10:15:08 -0300

    feat: definir rotas básicas de cadastro (GET cadastrar, POST salvar)

---
+++ cadastro-laravel/app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+  public function cadastrar()
+  {
+    return "Método cadastrar funcionando!";
+  }
+
+  public function salvar()
+  {
+    
+  }
+}
--- cadastro-laravel/routes/web.php
+++ cadastro-laravel/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsuarioController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [UsuarioController::class, 'cadastrar']);
+Route::post('/salvar', [UsuarioController::class, 'salvar']);
